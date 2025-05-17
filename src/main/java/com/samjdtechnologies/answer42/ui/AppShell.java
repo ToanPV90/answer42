54296ce808b3a9e41da42dafc1a80df1627d2c13
samjd-zz <shawn.dyck@protonmail.com>
2025-05-17 14:17:40 -0700

    Add Lumo to AppShell

--- src/main/java/com/samjdtechnologies/answer42/ui/AppShell.java
+++ src/main/java/com/samjdtechnologies/answer42/ui/AppShell.java
@@ -7,6 +7,8 @@ import com.vaadin.flow.component.page.Meta;
 import com.vaadin.flow.component.page.Viewport;
 import com.vaadin.flow.server.AppShellSettings;
 import com.vaadin.flow.server.PWA;
+import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.theme.lumo.Lumo;
 
 /**
  * Application shell configuration.
@@ -29,14 +31,17 @@ import com.vaadin.flow.server.PWA;
 @CssImport("./styles/themes/answer42/components/main-layout.css")
 @CssImport("./styles/themes/answer42/components/dashboard.css")
 @CssImport("./styles/themes/answer42/components/papers.css")
+@Theme(themeClass = Lumo.class)
 
 @JavaScript("./sw-loader.js")
 @JavaScript("./jwt-injector.js")
 @JavaScript("./sw-register.js")
+
 @Meta(name = "author", content = "SAMJD Technologies")
 @Meta(name = "application-name", content = "Answer42")
 @Meta(name = "apple-mobile-web-app-title", content = "Answer42")
 @Meta(name = "msapplication-TileColor", content="#6366f1")
+
 @Viewport("width=device-width, initial-scale=1.0")
 public class AppShell implements AppShellConfigurator {
     

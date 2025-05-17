0e983705095f8f5fdfffdcc882132d53fff55548
samjd-zz <shawn.dyck@protonmail.com>
2025-05-17 14:25:40 -0700

    Implement custom Answer42 theme with dark mode variant. Created a dedicated theme class that extends Lumo, added dark theme CSS file with specific component styling, and configured AppShell to use the custom theme and dark variant by default.

---
+++ src/main/java/com/samjdtechnologies/answer42/ui/theme/Answer42Theme.java
@@ -0,0 +1,16 @@
+package com.samjdtechnologies.answer42.ui.theme;
+
+import com.vaadin.flow.theme.lumo.Lumo;
+
+/**
+ * Answer42 custom theme that extends Lumo.
+ * This theme is used as the base theme for the application,
+ * with custom CSS applied from the frontend/styles/themes/answer42 directory.
+ */
+public class Answer42Theme extends Lumo {
+    public static final String DARK = "dark";
+    public static final String ANSWER42 = "answer42";
+    
+    // Name of the theme as it will be used in the @Theme annotation
+    public static final String THEME_NAME = "answer42";
+}
